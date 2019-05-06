300fbcd7aded909b3c3a53eeb1ba9f74de52a2c7
OrelBracha <orelbracha@yahoo.com>
2019-05-06 11:49:40 -0400

    edit

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -3,55 +3,95 @@
 #include <unordered_set>
 #include <math.h>
 #include <iostream>
-#define NUM_OF_DIGITS 10
+#include <bits/stdc++.h>
 
 using std::string, std::to_string, std::unordered_set;
-void SmartGuesser::allPermutations(){
-
-    uint options = pow(NUM_OF_DIGITS,this->length);
-    for(int i=0 ; i < options ; ++i)
-    {
-        string permutations = to_string(i);
-        while(permutations.length()<this->length)
-        {
-            permutations=to_string(0)+permutations;
-        }
-
-        _set.insert(permutations);
+/*
+void SmartGuesser::full_search(){
+//creates any possible guess
+//its sould take 10^(length)
+    uint END = pow(10,this->length);
+    for(int i=0;i<END;++i){
+        string s = to_string(i);
+        while(s.length()<this->length)
+            s=to_string(0)+s;//for any option under 10^(length-1)
+        _set.insert(s);
+		std::cout<<s<<std::endl;
     }
-}
+}*/
 
-string SmartGuesser::guess()
-{
 
+string SmartGuesser::guess() {
+    //guess any possible option
 	return this->Guess;
 }
 
 
 void SmartGuesser::startNewGame(uint Length) {
-    _set.clear();
+	Guess="";
+    _set.clear();//clear our set if it already full
+	tempguess="";
+	guess_index=0;
    this->length=Length;
-   this->allPermutations();
-   this->Guess = *_set.begin();
+  for(int i=0;i<this->length;i++)
+  Guess+='0';
+  //std::cout<<"guess is:"<<Guess<<std::endl;
 }
 
-void SmartGuesser::learn(string answer) {
-	char bulls= answer.at(0);
-	unordered_set<string> outFromGroup;
-	for ( auto it = _set.begin(); it != _set.end(); it++)
+void SmartGuesser::learn(string res) {
+	if(tempguess.length()<this->length){
+		int sum = (res.at(0)-'0');
+		for(int i=0;i<sum;i++)
+			tempguess+=to_string(guess_index);
+		guess_index++;
+		if(guess_index>9)
+			guess_index=0;
+		Guess_changer();
+	}
+	else
 	{
-        string result = bullpgia::calculateBullAndPgia(*it,Guess);
-
-		if (result.at(0)!=bulls)
-            outFromGroup.insert(*it);
+		if(_set.empty())
+			permute(tempguess);
+	
+	char bulls= res.at(0);//checks how many bulls we have
+	char cows= res.at(2);//checks how many cows we have
+	unordered_set<string> NotTheAnswer;
+	for ( auto it = _set.begin(); it != _set.end(); it++){
+        string response = bullpgia::calculateBullAndPgia(*it,Guess);
+			//std::cout<<"sum= "<<sum<<" tempsum= "<<tempsum<<std::endl;
+		if (response.at(0)!=bulls)
+			NotTheAnswer.insert(*it);	
 	}
 
-	//removes from myset
-	for ( auto it = outFromGroup.begin(); it != outFromGroup.end(); it++)
-    {
-        _set.erase(*it);
-    }
-
-    outFromGroup.clear();
+	//removes from _set
+	for ( auto it = NotTheAnswer.begin(); it != NotTheAnswer.end(); it++)
+		_set.erase(*it);
+		//NotTheAnswer.clear();
 		this->Guess= *_set.begin();
+	}
 }
+
+
+void SmartGuesser::Guess_changer(){
+	for(int i=0;i<this->length;i++){
+	//std::cout<<"guess index is "<<guess_index<<std::endl;
+	Guess.replace(i,i+1,to_string(guess_index));
+	//std::cout<<i<<"guess is:"<<Guess<<std::endl;
+	}
+}
+
+void SmartGuesser::permute(string str) 
+{ 
+    // Sort the string in lexicographically 
+    // ascennding order 
+    sort(str.begin(), str.end()); 
+  
+    // Keep printing next permutation while there 
+    // is next permutation 
+    do { 
+      _set.insert(str);
+	  //std::cout<<str<<std::endl;
+    } while (std::next_permutation(str.begin(), str.end())); 
+} 
+
+
